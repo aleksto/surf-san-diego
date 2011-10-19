e26a6c91ae1cd64a9101eefb08786de8782128bb
alitorstensen@gmail.com <alitorstensen@gmail.com@b46bd5ec-d5f2-c832-1ed6-0113792fb410>
2011-10-19 19:21:53 +0000

    Tester svn

---
+++ trunk/root/src/sql/MySQL.java
@@ -0,0 +1,76 @@
+package sql;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MySQL
+ */
+@WebServlet("/MySQL")
+public class MySQL extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MySQL() {
+        super();
+
+        
+        
+        Connection conn = null;
+        try {
+            conn =
+               DriverManager.getConnection("jdbc:mysql:localhost:3306/EXAMPLE?user=root&" +
+                                           "user=aleks&password=pothead");
+
+            	
+            // Do something with the Connection
+        } catch (SQLException ex) {
+            // handle any errors
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+        }
+        /*
+        ResultSet rset;
+		try {
+	        Statement stmt = conn.createStatement();
+			rset = stmt.executeQuery("SELECT pid FROM students");
+
+	        while(rset.next()){
+	        	System.out.println(rset.getInt(1));
+	        }
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+        */
+      
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter out = response.getWriter();
+		out.println("Hello World");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
