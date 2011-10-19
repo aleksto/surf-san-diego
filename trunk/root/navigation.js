a43cc5f13894f495e8d350843dc4f3996c6cc764
alitorstensen@gmail.com <alitorstensen@gmail.com@b46bd5ec-d5f2-c832-1ed6-0113792fb410>
2011-10-19 14:41:02 +0000

    Testing SVN

---
+++ trunk/root/navigation.js
@@ -0,0 +1,13 @@
+// JavaScript Document
+
+{
+	var menu = document.getElementById("nav").getElementsByTagName("a");
+	//creates an array out of each "a" element
+	for(var i=0; i<menu.length; i++) {
+		if(menu[i].href == "http://"+window.location.hostname+window.location.pathname) {
+		// comparing each href vs this allows for query and hash variables
+			menu[i].className="active";
+	   // add class of "active" if there is a match
+		}
+	}
+}
