6d305038d2a41d758941d7a2cdb8998c5b8c0c1e
alitorstensen@gmail.com <alitorstensen@gmail.com@b46bd5ec-d5f2-c832-1ed6-0113792fb410>
2011-10-19 14:40:15 +0000

    Testing SVN

--- trunk/WebContent/navigation.js
+++ trunk/WebContent/navigation.js
@@ -6,8 +6,7 @@
 	for(var i=0; i<menu.length; i++) {
 		if(menu[i].href == "http://"+window.location.hostname+window.location.pathname) {
 		// comparing each href vs this allows for query and hash variables
-			menu[i].className="active";
-	   // add class of "active" if there is a match
+		// add class of "active" if there is a match
 		}
 	}
 }
